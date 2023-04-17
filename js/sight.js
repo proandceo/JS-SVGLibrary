2eaec537ff6f53fefc0c96e8b4af1ee7e0dd3be0
Rex <proandceo@gmail.com>
2023-04-18 08:46:34 +1000

    Update sight.js

--- js/sight.js
+++ js/sight.js
@@ -1,1 +1,9 @@
-class SVGElement {}
+class SVGElement {
+  constructor(type) {
+    this.type = type;
+    this.namespace = "http://www.w3.org/2000/svg";
+    this.node = document.createElementNS(this.namespace, this.type);
+
+    return this;
+  }
+}
